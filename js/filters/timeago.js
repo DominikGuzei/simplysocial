3f0ab2f579eb364f8eb7e4a028eb81548b862014
Dominik Guzei <dominik.guzei@gmail.com>
2015-05-01 14:04:30 +0200

    fixes some timeago filter problems

--- js/filters/timeago.js
+++ js/filters/timeago.js
@@ -1,8 +1,7 @@
 SimplySocial.filter('timeago', function() {
   return function(date) {
 
-    var seconds = Math.floor((new Date() - date) / 1000);
-
+    var seconds = Math.floor((Date.now() - parseInt(date)) / 1000);
     var interval = Math.floor(seconds / 31536000);
     if (interval > 1) { return interval + " years"; }
 
